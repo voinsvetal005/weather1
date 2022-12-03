ba97310c636444798dc574c53699e7c9ecaf382b
voinsvetal005 <119705352+voinsvetal005@users.noreply.github.com>
2022-12-03 23:16:07 +0400

    Add files via upload

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -5,15 +5,20 @@ from config import tg_bot_token, open_weather_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-  
+
+
+#def send_messange():
+    # ваша функция отправки сообщений    
 channel_id = '-1001830691651' #Пробабота
 #channel_id = '-1001623252716' #Заповедные
-#channel_id = '-1001162004912' #Лосиный
 
+
+#channel_id = '-1001162004912' #Лосиный
 bot = Bot(token=tg_bot_token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 gorod='Mytishchi'
+#gorod='Moscow'
 now = datetime.datetime.today()
 NY = datetime.datetime(2023,1,2)
 d = NY-now #  str(d)  '83 days, 2:43:10.517807'
@@ -30,53 +35,52 @@ else:
     if d.days in d2:
         formaslova1 ="дня"
 
-code_to_smile = {
-    "Clear": "Ясно \U00002600",
-    "Clouds": "Облачно \U00002601",
-    "Rain": "Дождь \U00002614",
-    "Drizzle": "Дождь \U00002614",
-    "Thunderstorm": "Гроза \U000026A1",
-    "Snow": "Снег \U0001F328",
-    "Mist": "Туман \U0001F32B"
-    }
+f = open('december.txt', 'r', encoding='UTF-8')
+jokes = f.read().split('\n')
+f.close()
+# Пока не закончатся шутки, посылаем их в канал
 
+code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+        }
 @dp.message_handler()
 async def get_weather(message: types.Message):
-  
-    try:
-        r = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={gorod}&appid={open_weather_token}&units=metric"
-        )
-        data = r.json()
+    for joke in jokes:
+        try:
+            r = requests.get(
+                f"http://api.openweathermap.org/data/2.5/weather?q={gorod}&appid={open_weather_token}&units=metric"
+            )
+            data = r.json()
 
-        city = data["name"]
-        cur_weather = data["main"]["temp"]
+            city = data["name"]
+            cur_weather = data["main"]["temp"]
 
-        weather_description = data["weather"][0]["main"]
-        if weather_description in code_to_smile:
-            wd = code_to_smile[weather_description]
-        else:
-            wd = "Посмотри в окно, не пойму что там за погода!"
+            weather_description = data["weather"][0]["main"]
+            if weather_description in code_to_smile:
+                wd = code_to_smile[weather_description]
+            else:
+                wd = "Неясно"
 
-        humidity = data["main"]["humidity"]
-        pressure = int(data["main"]["pressure"]/1.33)
-        wind = data["wind"]["speed"]
-        
-        f = open('december.txt', 'r', encoding='UTF-8')
-        jokes = f.read().split('\n')
-        f.close()
-# Пока не закончатся шутки, посылаем их в канал
-        for joke in jokes:
-    # Делаем паузу в 86 400 сек
-            time.sleep(4)
-            await bot.send_photo(channel_id, 'https://photos.app.goo.gl/LN4QFvzdd9BVqo6r9', caption=f"Сегодня {datetime.datetime.now().strftime('%d-%m-%Y')}.\nДо Нового года {formaslova2}... {d.days} {formaslova1}!\n"
+            humidity = data["main"]["humidity"]
+            pressure = int(data["main"]["pressure"]/1.33)
+            wind = data["wind"]["speed"]
+
+            soros = joke.split('*')
+            
+            await bot.send_photo(channel_id, soros[1], caption=f"Сегодня {datetime.datetime.now().strftime('%d-%m-%Y')}.\nДо Нового года {formaslova2}... {d.days} {formaslova1}!\n"
               f"Погода в Лосином острове сейчас: \nТемпература: {cur_weather} C° {wd}\n"
               f"Влажность: {humidity}%  Давление: {pressure} мм.рт.ст  Ветер: {wind} м/с\n\n"
-              f"_{joke}_", parse_mode="Markdown" 
+              f"_{soros[0]}_", parse_mode="Markdown" 
               )
-    except:
-        await message.reply("\U00002620 Проверьте название города \U00002620") 
-#bot.send_message(channel_id, "Анекдоты закончились :-(")
-
+            
+        except:
+            await message.reply("\U00002620 Проверьте название города \U00002620")
+        time.sleep(600)
 if __name__ == '__main__':
     executor.start_polling(dp)
